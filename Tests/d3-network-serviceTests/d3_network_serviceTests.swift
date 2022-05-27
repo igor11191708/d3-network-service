739ce8a890490e8a7942c08068d933b03fafc824
Igor <igorshelopaev@gmail.com>
2022-05-27 17:49:32 +0300

    Update d3_network_serviceTests.swift

--- Tests/d3-network-serviceTests/d3_network_serviceTests.swift
+++ Tests/d3-network-serviceTests/d3_network_serviceTests.swift
@@ -6,6 +6,6 @@ final class d3_network_serviceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(d3_network_service().text, "Hello, World!")
+        //XCTAssertEqual(d3_network_service().text, "Hello, World!")
     }
 }
