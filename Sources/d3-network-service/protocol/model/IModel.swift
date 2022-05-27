9736a8bab64a769003501b5dd68fcf2a0dfba28a
Igor <igorshelopaev@gmail.com>
2022-05-27 15:51:13 +0300

    Update IModel.swift

--- Sources/d3-network-service/protocol/model/IModel.swift
+++ Sources/d3-network-service/protocol/model/IModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 /// Interface for models using as a data to exchange with a server
+@available(iOS 15, macOS 12.0, *)
 public protocol IModel: Codable, Identifiable, Hashable{
     
 }
