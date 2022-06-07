b546194f12c0dba353575a77b8839cdb5be1ba73
Igor <igorshelopaev@gmail.com>
2022-06-07 11:50:51 +0300

    Update Zipper.swift

--- Sources/d3-network-service/extension/combine/Zipper.swift
+++ Sources/d3-network-service/extension/combine/Zipper.swift
@@ -47,14 +47,14 @@ private extension Collection where Element: Publisher, Self.Index == Int {
             let c = a.count
 
             if let f = a.first {
-                if c == 1 {
-                    r += [f]
-                } else if c == 2 {
+                if c == 2 {
                     r += [p.Zip(f, a[1]).map { $0.0 + $0.1 }.erase()]
                 } else if c == 3 {
                     r += [p.Zip3(f, a[1], a[2]).map { $0.0 + $0.1 + $0.2 }.erase()]
                 } else if c == 4 {
                     r += [p.Zip4(f, a[1], a[2], a[3]).map { $0.0 + $0.1 + $0.2 + $0.3 }.erase()]
+                }else{
+                    r += [f]
                 }
             }
         }
